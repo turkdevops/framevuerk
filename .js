8a498d9ab74ffd5e5b9bb32b77470ecad1c80270
Kadir Selçuk <66804870+kadirselcuk@users.noreply.github.com>
2021-04-10 03:45:58 +0300

    Create .js

---
+++ .js
@@ -0,0 +1,13 @@
+{
+     " config-name " : " foobar " ,
+     " direction " : " ltr " ,
+     " birincil-renk " : " # 41b883 " ,
+     " ikincil-renk " : " # 35485d " ,
+     " tehlike-renk " : " # dd4b39 " ,
+     " uyarı-rengi " : "# ef8f00 " ,
+     "info-color " : " # 14b0cf " " bg renkli " : " #fff " " header-bg-color " : " # 35485d " " kenar çubuğu-bg-color " : " # 3a3a3a " " footer- bg-color " : " # 3a3a3a " ,
+     " padding " : " 1em " ,
+     "geçiş hızı " :" 0.35s " ,
+     " border-radius " : " 2px " ,
+     " shadow-size " : " 1px " 
+}
